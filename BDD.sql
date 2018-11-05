eb3e18f79475ce5e618cec6f537d3e3255a0692d
Cédric <cedric.leprohon@yahoo.fr>
2018-11-05 14:30:13 +0100

    SQL correction

--- BDD.sql
+++ BDD.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE comptabilite CHARACTER SET 'utf8';
 
+USE comptabilite;
+
 CREATE TABLE utilisateur(
 	id_utilisateur INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	mail VARCHAR (101) NOT NULL,
@@ -7,6 +9,15 @@ CREATE TABLE utilisateur(
 	PRIMARY KEY (id_utilisateur)
 )
 ENGINE=INNODB; 
+
+
+CREATE TABLE categorie(
+	id_categorie INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nom_categorie VARCHAR(50) NOT NULL,
+	type_transaction ENUM ('debit', 'credit'),
+	PRIMARY KEY (id_categorie)
+)
+ENGINE=INNODB;
 
 CREATE TABLE compte_bancaire(
 	id_cb INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -26,16 +37,9 @@ CREATE TABLE operation(
 	id_categorie INT UNSIGNED NOT NULL,
 	nom_operation VARCHAR(50) NOT NULL,
 	montant_operation FLOAT(100,2) NOT NULL,
-	date_operation DATE DEFAULT CURRENT_DATE,
+	date_operation DATETIME DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id_operation),
+	FOREIGN KEY (id_cb) REFERENCES compte_bancaire(id_cb) ON DELETE CASCADE,
 	FOREIGN KEY (id_categorie) REFERENCES categorie(id_categorie) ON DELETE CASCADE
-)
-ENGINE=INNODB;
-
-CREATE TABLE categorie(
-	id_categorie INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	nom_categorie VARCHAR(50) NOT NULL,
-	type_transaction ENUM ('débit', 'crédit'),
-	PRIMARY KEY (id_categorie)
 )
 ENGINE=INNODB;
