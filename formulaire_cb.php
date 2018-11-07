ef0c37c97da3d0a1857e94036c824abdc10564ee
Cédric <cedric.leprohon@yahoo.fr>
2018-11-07 11:51:09 +0100

    modification de compte

--- formulaire_cb.php
+++ formulaire_cb.php
@@ -4,6 +4,8 @@ require_once "fonctions/header.php";
 
 $bdd = connexion_bdd();
 
+
+
 if(isset($_POST['creation']))
 {
 
@@ -61,11 +63,63 @@ if(isset($_POST['creation']))
 
 }
 
+if(isset($_GET['supprimer'])){
+	
+	$id_cb = intval($_GET['supprimer']);
+
+	if(is_int($id_cb))
+	{
+		$req = $bdd->prepare("DELETE FROM compte_bancaire
+		WHERE id_cb = :id_cb AND id_utilisateur = :id_utilisateur;");
+		$req->execute(array(
+			"id_cb" => $id_cb,
+			"id_utilisateur" => $_SESSION['id']
+		));
+
+		echo "Le compte a été supprimé.";
+	}
+
+}
+
+$edit = false;
+$mon_compte = NULL;
+
+if(isset($_GET['editer'])){
+
+	$id_cb1 = intval($_GET['editer']);
+
+	if(is_int($id_cb1))
+	{
+		// $req = $bdd->prepare ("UPDATE compte_bancaire SET nom_compte = :nom_compte 
+		// WHERE id_cb = :id_cb AND id_utilisateur = :id_utilisateur");
+		// $req->execute (array(
+		// 	"nom_compte" => "Jarno Bank",
+		// 	"id_cb" => $id_cb1,
+		// 	"id_utilisateur" => $_SESSION['id']
+		// ));
+
+		// echo "Le compte a été édité.";
+
+		$edit = true;
+
+		$req = $bdd->prepare("SELECT nom_compte FROM compte_bancaire WHERE id_cb = ?");
+		$req->execute(array($id_cb1));
+
+		$data = $req->fetch();
+		$mon_compte = $data['nom_compte'];
+
+	}
+}
+
 
 ?>
 <h1> Créer un compte bancaire virtuel </h1>
 <form method="POST" action="formulaire_cb.php">
-	<div class="nom_du_compte"> Nom du compte <input type="text" name="nom_du_compte" placeholder="Nom du compte"> </div>
+	<div class="nom_du_compte"> Nom du compte <input type="text" name="nom_du_compte" value="<?= $mon_compte; ?>" placeholder="Nom du compte"> </div>
+	<?php 
+	if($edit == false)
+	{
+	?>
 	<div class="type_de_compte"> Type de compte <select name="type_de_compte">
 		<option value="courant"> Courant </option>
 		<option value="epargne"> Epargne </option>
@@ -75,6 +129,9 @@ if(isset($_POST['creation']))
 	<div class="devise"> USD <input type="radio" name="devise" value="USD">
 						 EUR <input type="radio" name="devise" value="EUR">
 	</div>
+	<?php
+	}
+	?>
 	<input type="submit" value="JDF" name="creation">
 </form>
 <hr />
